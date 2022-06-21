529e04b8a5c0fe244541481242c52e0da722ef87
Jazo <jazoluxetveritas@gmail.com>
2022-06-20 22:43:30 -0500

    styles css component

--- src/PooProject.js
+++ src/PooProject.js
@@ -10,6 +10,38 @@ export class PooProject extends LitElement {
 
   static get styles() {
     return css`
+       :host {
+        display: block;
+        background-color: #f9f9fd;
+
+background-image:  linear-gradient(30deg, #000000 12%, transparent 12.5%, transparent 87%, #000000 87.5%, #000000), linear-gradient(150deg, #000000 12%, transparent 12.5%, transparent 87%, #000000 87.5%, #000000), linear-gradient(30deg, #000000 12%, transparent 12.5%, transparent 87%, #000000 87.5%, #000000), linear-gradient(150deg, #000000 12%, transparent 12.5%, transparent 87%, #000000 87.5%, #000000), linear-gradient(60deg, #00000077 25%, transparent 25.5%, transparent 75%, #00000077 75%, #00000077), linear-gradient(60deg, #00000077 25%, transparent 25.5%, transparent 75%, #00000077 75%, #00000077);
+background-size: 22px 39px;
+background-position: 0 0, 0 0, 11px 19px, 11px 19px, 0 0, 11px 19px;
+       }
+       .container {
+        text-align: center;
+       }
+       .card{
+        background-color: #e4e4e4;
+        border-radius: 25px;
+        display: inline-block;
+        align-content: center;
+        align-items: center;
+        text-align: center;
+        flex-direction: column;
+        margin: 12px 5px 12px 5px;
+        padding:  10px 0 10px 0;
+        width: 200px;
+        height: 300px;
+        box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.25);
+      }
+      get-data{
+        display:none;
+      }
+      .card img {
+        border-radius: 50%;
+        width: 70%;
+      }
       
     `;
   }
@@ -49,17 +81,15 @@ export class PooProject extends LitElement {
       ${this.wiki.map(character => html `
          <div class="card">
           <div class="card-container">
-            <img src="${character.image}">
+            <img class="card-img" src="${character.image}">
             <h2>${character.name}</h2>
-            <p>${character.species | character.status}</p>
+            <p>${character.species}</p>
+            <p>${character.status}</p>
           </div>
          </div>
       ` )}
      
     `;
   }
-
-
-
-}
+};
 
